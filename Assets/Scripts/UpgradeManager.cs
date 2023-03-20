9ff187ab0bf0d453ec3e1da34fbda7240cf7c79b
Tharurtius <michael.yee6@studytafensw.edu.au>
2023-03-20 22:53:37 +1100

    upgrades screen

--- Assets/Scripts/UpgradeManager.cs
+++ Assets/Scripts/UpgradeManager.cs
@@ -1,15 +1,28 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class UpgradeManager : MonoBehaviour
 {
+    [SerializeField] private Button speed;
+    [SerializeField] private Button foam;
+    [SerializeField] private Button cooldown;
+    [SerializeField] private Text cost;
+    [SerializeField] private Text gold;
+
+    [SerializeField] private GameObject pausePanel;
+    [SerializeField] private GameObject upgradePanel;
     public void Speed()
     {
         if (GameManager.cash >= GameManager.cost)
         {
             GameManager.cash -= GameManager.cost;
             GameManager.Singleton.UpgradeSpeed();
+        }
+        else
+        {
+            DisableButton(speed);
         }
     }
 
@@ -20,6 +33,10 @@ public class UpgradeManager : MonoBehaviour
             GameManager.cash -= GameManager.cost;
             GameManager.Singleton.UpgradeFoam();
         }
+        else
+        {
+            DisableButton(foam);
+        }
     }
 
     public void Cooldown()
@@ -29,5 +46,28 @@ public class UpgradeManager : MonoBehaviour
             GameManager.cash -= GameManager.cost;
             GameManager.Singleton.UpgradeCooldown();
         }
+        else
+        {
+            DisableButton(cooldown);
+        }
+    }
+
+    public void DisableButton(Button button)
+    {
+        button.interactable = false;
+
+        button.GetComponentInChildren<Text>().text = "Can't afford this";
+    }
+
+    public void SetupUI()
+    {
+        cost.text = GameManager.cost.ToString("N0");
+        gold.text = "Gold :" + GameManager.cash.ToString("N0");
+    }
+
+    public void UpgradeScreen()
+    {
+        upgradePanel.SetActive(true);
+        pausePanel.SetActive(false);
     }
 }
